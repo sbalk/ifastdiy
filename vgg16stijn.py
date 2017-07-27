ced6b1ed96399954fa52b97bd3f614a7d7366c63
Ubuntu <stijn.balk@gmail.com>
2017-07-27 16:33:14 +0000

    add test

--- vgg16stijn.py
+++ vgg16stijn.py
@@ -139,3 +139,19 @@ class Vgg16Stijn():
             latest_weights_filename = 'ft%d%s.h5' %(epoch+1, extra_info)
             self.model.save_weights(results_path+latest_weights_filename)
         print "Completed %s fit operations" % nb_epoch
+    
+    def test(self, path, batch_size=8):
+        """
+            Predicts the classes using the trained model on data yielded batch-by-batch.
+
+            Args:
+                path (string):  Path to the target directory. It should contain one subdirectory 
+                                per class.
+                batch_size (int): The number of images to be considered in each batch.
+            
+            Returns:
+                test_batches, numpy array(s) of predictions for the test_batches.
+    
+        """
+        test_batches = self.batch_iterator(path, shuffle=False, batch_size=batch_size, class_mode=None)
+        return test_batches, self.model.predict_generator(test_batches, test_batches.nb_sample)
